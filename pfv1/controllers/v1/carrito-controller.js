6cf88d8a1d86ff31c79ff429d8933827ef2c29a2
Gian Beretti <80936118+gianfrancoberetti@users.noreply.github.com>
2021-08-07 10:57:31 -0300

    Add files via upload

---
+++ pfv1/controllers/v1/carrito-controller.js
@@ -0,0 +1,21 @@
+const listaCarrito = (req, res) => {
+    res.json('probando')
+}
+const agregarAlCarrito = (req, res) => {
+    res.send('probando');
+}
+const borrarDelCarrito = (req, res) => {
+    res.send('probando');
+}
+
+
+
+
+
+
+
+module.exports = {
+    listaCarrito,
+    agregarAlCarrito,
+    borrarDelCarrito
+}
